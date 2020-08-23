f9ffcfd45ad4c690b0c6b0a4db9fb918224b5be9
gabrielrumiranda <gabrielrumiranda@gmail.com>
2020-08-23 15:13:30 -0300

    Fixing travis script

--- config/database.js
+++ config/database.js
@@ -8,20 +8,20 @@ module.exports = {
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_DEVELOPMENT_NAME,
     host: process.env.DATABASE_HOST,
-    dialect: process.env.DATABASE_DIALECT
+    dialect: process.env.DATABASE_DIALECT || 'postgres'
   },
   test: {
     username: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_TEST_NAME,
     host: process.env.DATABASE_HOST,
-    dialect: process.env.DATABASE_DIALECT
+    dialect: process.env.DATABASE_DIALECT || 'postgres'
   },
   production: {
     username: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_PRODUCTION_NAME,
     host: process.env.DATABASE_HOST,
-    dialect: process.env.DATABASE_DIALECT
+    dialect: process.env.DATABASE_DIALECT || 'postgres'
   }
 };
